60521a8e7ca897354d699f30949d99230511c58a
Rafael Medrano <rafael.medrano@aitesol.com>
2017-09-30 22:19:14 -0500

    Paso 2: Creacion de prueba para validar un string de un numero para la funcion de numero de elementos (Rojo)

--- EstadisticaTest.py
+++ EstadisticaTest.py
@@ -3,5 +3,8 @@ from unittest import TestCase
 from Estadistica import Estadistica
 
 class TestEstadistica(TestCase):
+
     def test_numeroElementos(self):
         self.assertEqual(Estadistica().numeroElementos( "" ),0,"Cadena vacia")
+    def test_numeroElementosUnNumero(self):
+        self.assertEqual(Estadistica().numeroElementos("1"), 1, "Un numero")
