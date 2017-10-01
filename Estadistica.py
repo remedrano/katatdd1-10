0a07eef64b2fe4df7f1d5eaed31a483afc8aec96
Rafael Medrano <rafael.medrano@aitesol.com>
2017-09-30 22:30:24 -0500

    Paso 4: Refactor de la funcion de numero de elementos (Azul)

--- Estadistica.py
+++ Estadistica.py
@@ -3,13 +3,9 @@ class Estadistica:
     def numeroElementos(self, cadena):
         if (cadena == ""):
             return 0
-        elif len(cadena) > 3:
+        else:
             numeros = cadena.split(",")
             cantidad = 0
             for num in numeros:
                 cantidad += 1
             return cantidad
-        elif ("," in cadena):
-            return 2
-        else:
-            return 1
