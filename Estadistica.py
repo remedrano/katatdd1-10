68db24e15ff762a712d76ec18ab6987ea609bc2d
Rafael Medrano <rafael.medrano@aitesol.com>
2017-09-30 22:22:47 -0500

    Paso 3: Modificacion de codigo para pasar prueba de validacion de un string de dos numeros para la funcion de numero de elementos (Verde)

--- Estadistica.py
+++ Estadistica.py
@@ -3,5 +3,7 @@ class Estadistica:
     def numeroElementos(self, cadena):
         if (cadena == ""):
             return 0
+        elif ("," in cadena):
+            return 2
         else:
             return 1
