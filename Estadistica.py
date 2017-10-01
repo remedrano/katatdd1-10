4ae308ee4cb44bfcaf12edd9ce4f2eb758c122e5
Rafael Medrano <rafael.medrano@aitesol.com>
2017-09-30 22:25:55 -0500

    Paso 4: Modificacion de codigo para pasar prueba de validacion de un string de N numeros para la funcion de numero de elementos (Verde)

--- Estadistica.py
+++ Estadistica.py
@@ -3,6 +3,12 @@ class Estadistica:
     def numeroElementos(self, cadena):
         if (cadena == ""):
             return 0
+        elif len(cadena) > 3:
+            numeros = cadena.split(",")
+            cantidad = 0
+            for num in numeros:
+                cantidad += 1
+            return cantidad
         elif ("," in cadena):
             return 2
         else:
